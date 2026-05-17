b286f88d5ec52e85af64f6010c5a21215ebf4100
wellington.anaral <well.amaral2007@gmail.com>
2026-05-17 13:59:16 -0300

    adjusts

--- src/BaseFramework/DAL/AbstractCRUDDAL.cs
+++ src/BaseFramework/DAL/AbstractCRUDDAL.cs
@@ -22,7 +22,6 @@ public abstract class AbstractCRUDDAL : Component, IDAL<IEntity>
     public AbstractCRUDDAL()
         : base()
     {
-        
         var sqlConnectionString = "Server=GCP_CLOUD_SQL\\SQLEXPRESS;Database=FinanceDb;Trusted_Connection=True;TrustServerCertificate=True;";
         session = new(sqlConnectionString);
     }
